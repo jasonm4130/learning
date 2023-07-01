e25b4c5c197d3dd4494b5e3c31d12f40532181b2
Jason Matthew <jasonm4130@gmail.com>
2023-07-01 18:37:20 +1000

    feat: added leetcode as platform and setup typescript, completed hello world problem

---
+++ Platforms/Leetcode/TypeScript/Easy/2667 - Create Hello World Function.ts
@@ -0,0 +1,13 @@
+/**
+ * @return {Function}
+ */
+function createHelloWorld() {
+  return function (...args: any[]) {
+    return "Hello World";
+  };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
