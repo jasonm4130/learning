3ec5064a15100cfab6020a811af81959c18146ce
Jason Matthew <jasonm4130@gmail.com>
2023-07-02 09:07:50 +1000

    fix: updated promise function to use race

--- Platforms/Leetcode/TypeScript/Easy/2637 - Promise Time Limit.ts
+++ Platforms/Leetcode/TypeScript/Easy/2637 - Promise Time Limit.ts
@@ -4,13 +4,15 @@ type Fn = (...params: any[]) => Promise<any>;
 function timeLimit(fn: Fn, t: number): Fn {
   return async function (...args: number[]): Promise<any> {
     // Return a promise that resolves after the specified number of milliseconds
-    const promise = new Promise((resolve, reject) => {
+    const timeout = new Promise((resolve, reject) => {
       setTimeout(() => reject('Time Limit Exceeded'), t);
+    });
 
+    const result = new Promise((resolve, reject) => {
       fn(...args).then(resolve, reject);
     });
 
     // Return the result of calling the function
-    return promise;
+    return Promise.race([result, timeout]);
   };
 }
